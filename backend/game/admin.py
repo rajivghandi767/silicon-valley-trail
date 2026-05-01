648808deaca8b610540da19b81bd9edc224b2913
Rajiv Wallace <rajivghandi972@gmail.com>
2026-05-01 14:28:19 -0400

    refactor: remove GameState model and associated admin configuration

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Location, GameState
+from .models import Location
 
 
 @admin.register(Location)
@@ -7,10 +7,3 @@ class LocationAdmin(admin.ModelAdmin):
     list_display = ('sequence_in_journey', 'name', 'latitude', 'longitude')
     ordering = ('sequence_in_journey',)
     search_fields = ('name',)
-
-
-@admin.register(GameState)
-class GameStateAdmin(admin.ModelAdmin):
-    list_display = ('id', 'current_location',
-                    'days_remaining', 'cash', 'bugs', 'morale')
-    list_filter = ('days_remaining', 'bugs')
