368552faedf05af353f92c094c6f4417e86aec3e
Rajiv Wallace <rajivghandi972@gmail.com>
2026-06-09 13:28:30 -0400

    chore: remove django-redis and standardize on native django cache

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -38,11 +38,8 @@ DATABASES = {
 
 CACHES = {
     'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
         'LOCATION': os.environ.get('REDIS_URL'),
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
     }
 }
 
